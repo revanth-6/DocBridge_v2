b9b3e71541a0432d4ee96647230044c422260a50
revanth-6 <307519@ust.com>
2026-06-18 22:47:32 +0530

    fix: refactor keyvault module to use inline access policies to avoid bootstrap issues

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -26,32 +26,28 @@ resource "azurerm_key_vault" "main" {
     virtual_network_subnet_ids = []
   }
 
+  # Access Policy for Terraform Service Principal (Deployer)
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get", "List", "Set", "Delete", "Purge", "Recover"
+    ]
+  }
+
+  # Access Policy for AKS Workload Identity
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = var.aks_workload_identity_oid
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
+
   tags = var.tags
 }
 
-# 2. Access Policy for Terraform Service Principal (Deployer)
-resource "azurerm_key_vault_access_policy" "terraform_sp" {
-  key_vault_id = azurerm_key_vault.main.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get", "List", "Set", "Delete", "Purge", "Recover"
-  ]
-}
-
-# 3. Access Policy for AKS Workload Identity
-resource "azurerm_key_vault_access_policy" "aks_workload" {
-  key_vault_id = azurerm_key_vault.main.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = var.aks_workload_identity_oid
-
-  secret_permissions = [
-    "Get", "List"
-  ]
-}
-
-# 4. Private Endpoint
+# 2. Private Endpoint
 resource "azurerm_private_endpoint" "kv" {
   name                = "${var.project}-${var.environment}-kv-pe"
   location            = var.location
@@ -67,7 +63,7 @@ resource "azurerm_private_endpoint" "kv" {
   }
 }
 
-# 5. DNS A Record
+# 3. DNS A Record
 resource "azurerm_private_dns_a_record" "kv" {
   name                = local.vault_name
   zone_name           = "privatelink.vaultcore.azure.net"
@@ -76,7 +72,7 @@ resource "azurerm_private_dns_a_record" "kv" {
   records             = [azurerm_private_endpoint.kv.private_service_connection[0].private_ip_address]
 }
 
-# 6. Store Secrets in Key Vault using for_each
+# 4. Store Secrets in Key Vault using for_each
 locals {
   secrets_map = {
     "db-password"             = var.db_password
@@ -95,6 +91,6 @@ resource "azurerm_key_vault_secret" "secrets" {
 
   # Ensure access policies are applied before writing secrets
   depends_on = [
-    azurerm_key_vault_access_policy.terraform_sp
+    azurerm_key_vault.main
   ]
 }
