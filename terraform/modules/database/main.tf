4bee731243927aa7d6bb555ed19eff87f51c8179
revanth-6 <307519@ust.com>
2026-06-18 22:01:30 +0530

    chore: format modules/database/main.tf with terraform fmt

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -7,16 +7,16 @@ locals {
 
 # 1. PostgreSQL Flexible Server
 resource "azurerm_postgresql_flexible_server" "main" {
-  name                   = local.server_name
-  resource_group_name    = local.resource_group_name
-  location               = var.location
-  version                = "15"
-  delegated_subnet_id    = var.database_subnet_id
-  private_dns_zone_id    = var.postgres_dns_zone_id
+  name                          = local.server_name
+  resource_group_name           = local.resource_group_name
+  location                      = var.location
+  version                       = "15"
+  delegated_subnet_id           = var.database_subnet_id
+  private_dns_zone_id           = var.postgres_dns_zone_id
   public_network_access_enabled = false
-  administrator_login    = "docbridge_user"
-  administrator_password = var.db_password
-  zone                   = "1"
+  administrator_login           = "docbridge_user"
+  administrator_password        = var.db_password
+  zone                          = "1"
 
   storage_mb = 32768
   sku_name   = "B_Standard_B2s"
